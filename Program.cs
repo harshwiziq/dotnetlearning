4662b243abe8f509c848065c1f57fdb029783658
Harsh Kumar <Harsh.Kumar@soti.net>
2019-07-06 21:04:54 +0530

    First Checkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetlearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
